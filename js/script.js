4753f697b4542e8c10ddd59850a894eeaa014b6b
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-02-09 10:50:28 +0500

    screenshot

--- js/script.js
+++ js/script.js
@@ -21,7 +21,19 @@ window.onscroll = () => {
 
 
 /*========== swiper ==========*/
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 /*========== dark light mode ==========*/
 
